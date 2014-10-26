ee1d648d0fd2dcb708f73e31783ed210d18b5fa0
auronff10 <ledesmax@gmail.com>
2014-10-26 17:41:49 +0100

    Modificado client.py para añadir el expires y modificar la entrada

--- client.py
+++ client.py
@@ -8,29 +8,30 @@ import socket
 import sys
 
 # Cliente UDP simple.
-
+try:
 # Dirección IP del servidor.
-SERVER = sys.argv[1]
-PORT = int(sys.argv[2])
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
 
-if sys.argv[3] == "register":
-    LINE = "REGISTER " + "sip:" + sys.argv[4] + " SIP/2.0\r\n\r\n"
-else:
+    if sys.argv[3] == "register":
+        LINE = "REGISTER " + "sip:" + \
+            sys.argv[4] + " SIP/2.0\r\n\r\n" + \
+            "Expires: " + sys.argv[5] + "\r\n\r\n"
 
-    LINE = " ".join(sys.argv[3:])
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((SERVER, PORT))
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+    print "Enviando: " + LINE
+    my_socket.send(LINE + '\r\n')
+    data = my_socket.recv(1024)
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
+    print 'Recibido -- ', data
+    print "Terminando socket..."
 
-print 'Recibido -- ', data
-print "Terminando socket..."
-
-# Cerramos todo
-my_socket.close()
-print "Fin."
+    # Cerramos todo
+    my_socket.close()
+    print "Fin."
+except:
+    print "Usage: client.py ip puerto register sip_address expires_value"
