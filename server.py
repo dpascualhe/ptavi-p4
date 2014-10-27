eb948130d86df4b99bb9c9c2840303e7c8c52321
raquelpt <raquelita_25_4@hotmail.com>
2014-10-27 15:19:38 +0100

    Update server.py

--- server.py
+++ server.py
@@ -14,52 +14,56 @@ port = ast.literal_eval(sys.argv[1])
 fichero = 'registered.txt'
 
 def register2file(fichero, dicc):
+ """
+   Imprimir el diccionario de clientes
+ """
 
-	fich = open(fichero, 'w')
-	for cliente in dicc.keys():
-		ip = dicc[user][0]
-		tiempo = dicc[user][1]
-		str_time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(tiempo))
-		texto = cliente + '\t' + ip + '\t' + str_time + '\r\n'
-		fich.write(texto)
-	fich.close()
+    fich = open(fichero, 'w')
+    for cliente in dicc.keys():
+        ip = dicc[user][0]
+        tiempo = dicc[user][1]
+        str_time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(tiempo))
+        texto = cliente + '\t' + ip + '\t' + str_time + '\r\n'
+        fich.write(texto)
+    fich.close()
 
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
-    Echo server class
+    SIPRegisterHandler clase
     """
 
-	ListCliente = {}
+    ListCliente = {}
     def handle(self):
 
-		for i in self.ListCliente.keys():
-			if self.ListCliente[i][1] < time.time():
-				del self.ListCliente[i]
-				print self.ListCliente
+        for i in self.ListCliente.keys():
+            if self.ListCliente[i][1] < time.time(): # comparando tiempo de
+                del self.ListCliente[i] #expirar y borrar cliente
+                print self.ListCliente
 
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write("Hemos recibido tu peticion")
-		datos_clientes = list(self.client_address)
-		print datos_clientes
+        datos_clientes = list(self.client_address)
+        print datos_clientes
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             lista = self.rfile.read()
-			print lista
-			lista = lista.split()
-			correo = lista[2]
-			ip = datos_clientes(0)
-		    if lista[0] == 'REGISTER':
-				if lista[6] == '0':
-					if correo in self.ListCliente:
-						del self.ListCliente[correo]
-					register2file(fichero, ListCliente)
-				else:
-					self.ListCliente[correo] = (ip, time.time()+float(lista[6]))
-					register2file(fichero, ListCliente)
-				self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
-			
-			if not lista:
-				break
+            print lista
+            lista = lista.split()
+            correo = lista[2]
+            ip = datos_clientes(0)
+            if lista[0] == 'REGISTER':
+                if lista[6] == '0': # comprobamos el expirar
+                    if correo in self.ListCliente:
+                        del self.ListCliente[correo] # borramos cliente
+                    register2file(fichero, ListCliente)
+                else:
+                    self.ListCliente[correo] = (ip, time.time()+float(lista[6]))
+                    # añadimos cliente
+                    register2file(fichero, ListCliente) # imprimimos diccionario
+                self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
+            
+            if not lista:
+                break
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
