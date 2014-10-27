14dbb8d3d177d8819ff0c55014241dfe9a15a9d7
david pascual hernandez <dpascual@iota18.aulas.gsyc.es>
2014-10-27 09:29:59 +0100

    es capaz de mandar REGISTER

--- client.py
+++ client.py
@@ -17,17 +17,23 @@ PORT = int(sys.argv[2])
 # Contenido que vamos a enviar
 LINE = ""
 for word in sys.argv[3:]:
-        LINE += word + " "
+        # Comprobamos si tenemos un 'register'
+        if sys.argv[3] = 'register'
+                LINE = 'REGISTER sip:' + sys.argv[4] + ' SIP/2.0\r\n\r\n'
+        else
+                LINE += word + " "
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
+# Enviamos un string
 print "Enviando: " + LINE
 my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
 
+# Recibimos datos del servidor
+data = my_socket.recv(1024)
 print 'Recibido -- ', data
 print "Terminando socket..."
 
