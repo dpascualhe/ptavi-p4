cb8eedf452556a26e486a2f0378e95b0054abb88
Omar Alejandro Ledesma Villacis <omarled@iota27.aulas.gsyc.es>
2014-10-27 20:17:08 +0100

    Cambios menores, indexError

--- client.py
+++ client.py
@@ -8,30 +8,36 @@ import socket
 import sys
 
 # Cliente UDP simple.
-try:
+
 # Dirección IP del servidor.
+try:
     SERVER = sys.argv[1]
     PORT = int(sys.argv[2])
 
     if sys.argv[3] == "register":
-        LINE = "REGISTER " + "sip:" + \
-            sys.argv[4] + " SIP/2.0\r\n\r\n" + \
-            "Expires: " + sys.argv[5] + "\r\n\r\n"
+        LINE = "REGISTER" + " sip:" + sys.argv[4] + " SIP/2.0\r\n" + \
+        "Expires: " + sys.argv[5] + "\r\n\r\n"
+    else:
 
-    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+        linea = sys.argv[3:]
+        LINE = ""
+        iteracion = 0
+        for palabra in linea:
+            iteracion = iteracion + 1
+            if iteracion == 1:
+                LINE = LINE + palabra
+            else:
+                LINE = LINE + " " + palabra
+
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
-
     print "Enviando: " + LINE
     my_socket.send(LINE + '\r\n')
     data = my_socket.recv(1024)
-
     print 'Recibido -- ', data
     print "Terminando socket..."
-
-    # Cerramos todo
     my_socket.close()
     print "Fin."
-except:
+except IndexError:
     print "Usage: client.py ip puerto register sip_address expires_value"
