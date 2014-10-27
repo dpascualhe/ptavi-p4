fbb4c4185468a3cb892e3d173593c34d7f1d894f
david pascual hernandez <dpascual@iota18.aulas.gsyc.es>
2014-10-27 10:38:35 +0100

    tratamos con cabecera 'expires'

--- client.py
+++ client.py
@@ -15,13 +15,11 @@ SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 
 # Contenido que vamos a enviar
-LINE = ""
+PETICION = ""
 for word in sys.argv[3:]:
-        # Comprobamos si tenemos un 'register'
-        if sys.argv[3] = 'register'
-                LINE = 'REGISTER sip:' + sys.argv[4] + ' SIP/2.0\r\n\r\n'
-        else
-                LINE += word + " "
+    # Formamos la peticion
+    PETICION = "REGISTER sip:" + sys.argv[4] + " SIP/2.0\r\n"
+    PETICION += "Expires: " + sys.argv[5] + "\r\n"
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -29,8 +27,8 @@ my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 # Enviamos un string
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
+print "Enviando: " + PETICION
+my_socket.send(PETICION + '\r\n')
 
 # Recibimos datos del servidor
 data = my_socket.recv(1024)
--- server.py
+++ server.py
@@ -14,21 +14,25 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     
     def handle(self):
-        # Inicializamos el diccionario de clientes
-        clients = {}
         while 1:
-            # Leemos lo que nos envía el cliente
-            line_string = self.rfile.read()
-            line = line_string.split(" ")
+            # Leemos lo que nos envía el cliente y lo separamos en lineas
+            peticion_string = self.rfile.read()
+            peticion_lines = peticion_string.split("\r\n")
+            peticion = peticion_lines[0].split(" ")
             # Procesamos el REGISTER
-            if line[0] == "REGISTER":
-                sip_dir = line[1].split(":")[1]
+            if peticion[0] == "REGISTER":
+                sip_dir = peticion[1].split(":")[1]
                 clients[sip_dir] = self.client_address[0]
+                # Procesamos la cabecera 'expires'
+                cabecera = peticion_lines[1]
+                expire_time = cabecera.split(" ")[1]
+                if expire_time == '0':
+                    del clients[sip_dir]                
                 # Enviamos OK 
                 self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
-            if not line_string:
+            if not peticion_string:
                 break
-            print "El cliente nos manda " + line_string
+            print "El cliente nos manda " + peticion_string
         # Imprimimos la dirección del cliente       
         print self.client_address
         print clients
@@ -40,5 +44,7 @@ PORT = int(sys.argv[1])
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     print "Lanzando servidor UDP de eco..."
+    #Inicializamos el diccionario de clientes
+    clients = {}
     serv = SocketServer.UDPServer(("", PORT), SIPRegisterHandler)
     serv.serve_forever()
