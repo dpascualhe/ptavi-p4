78f2d52dd571505a1aa31b783c0609b2d90ae1d3
david pascual hernandez <dpascual@iota02.aulas.gsyc.es>
2014-10-27 15:34:27 +0100

    guardamos en registered.txt los clientes

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ en UDP simple
 
 import SocketServer
 import sys
+import time
 
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
@@ -19,23 +20,42 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
             peticion_string = self.rfile.read()
             peticion_lines = peticion_string.split("\r\n")
             peticion = peticion_lines[0].split(" ")
+           
             # Procesamos el REGISTER
             if peticion[0] == "REGISTER":
-                sip_dir = peticion[1].split(":")[1]
-                clients[sip_dir] = self.client_address[0]
+                self.sip_dir = peticion[1].split(":")[1]
+                clients[self.sip_dir] = self.client_address[0]
                 # Procesamos la cabecera 'expires'
                 cabecera = peticion_lines[1]
-                expire_time = cabecera.split(" ")[1]
-                if expire_time == '0':
-                    del clients[sip_dir]                
+                self.expire_sec = int(cabecera.split(" ")[1])
+                if self.expire_sec == 0:
+                    del clients[self.sip_dir]                
                 # Enviamos OK 
                 self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
+            
             if not peticion_string:
                 break
             print "El cliente nos manda " + peticion_string
+            
         # Imprimimos la dirección del cliente       
         print self.client_address
         print clients
+
+        self.register2file()
+    
+    # Lleva un registro de los clientes conectados
+    def register2file(self):
+        # Abrimos el fichero...
+        fich = open('registered.txt','w')
+        fich.write('User\tIP\tExpires\r\n')
+            
+        # Obtenemos el tiempo de expiración
+        expire = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()+self.expire_sec))
+        clients[self.sip_dir] = [self.client_address[0], expire]
+        
+        for client in clients.keys():
+            fich.write(client + '\t' + clients[client][0] + '\t' + clients[client][1] + '\r\n') 
+        fich.close()       
             
 
 # Extraemos el puerto de los argumentos
@@ -43,8 +63,11 @@ PORT = int(sys.argv[1])
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    print "Lanzando servidor UDP de eco..."
-    #Inicializamos el diccionario de clientes
+    print "Lanzando servidor SIP register..."
+
+    #Inicializamos los diccionarios de clientes
     clients = {}
+    clients_time = {}
+
     serv = SocketServer.UDPServer(("", PORT), SIPRegisterHandler)
     serv.serve_forever()
